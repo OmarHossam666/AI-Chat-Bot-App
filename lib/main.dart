d78de7922e058a4e7971ea0284807fc1569d8a39
(-_-)_Omar_Hossam_(-_-) <153639313+OmarHossam666@users.noreply.github.com>
2024-11-30 20:50:53 +0200

    Adding logo & name to the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ai_assistant/home_page.dart';
+import 'package:ai_assistant/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
